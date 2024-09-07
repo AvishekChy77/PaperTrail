ec72e4cc37ab6b503c144555f0fab30feb01ec80
avishek <avishekchowdhury71@gmail.com>
2024-09-07 14:51:11 +0600

    added NoteForm

--- src/Pages/NewNote/NewNote.tsx
+++ src/Pages/NewNote/NewNote.tsx
@@ -1,7 +1,9 @@
+import NoteForm from "../../Component/NoteForm.jsx"
 const NewNote = () => {
   return (
     <div>
       <h1 className="mb-4">New Note</h1>
+      <NoteForm />
     </div>
   );
 };

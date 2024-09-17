58b920a78ea47e4219c60b269333a033a808df7c
AvishekChy77 <avishekchowdhury71@gmail.com>
2024-09-18 01:01:03 +0600

    added Note details page

--- src/Pages/ShowNote/Note.tsx
+++ src/Pages/ShowNote/Note.tsx
@@ -1,23 +1,39 @@
-import { Badge, Col, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
+import { NavLink } from "react-router-dom";
 import useNote from "../../Hooks/useNote";
 
 const Note = () => {
   const note = useNote();
   return (
-    <Row className="align-items-center mb-4">
-      <Col>
-        <h1>{note.title}</h1>
-        {note.tags.length > 0 && (
-          <Stack gap={1} direction="horizontal" className="flex-wrap">
-            {note.tags.map((tag) => (
-              <Badge className="text-truncates" key={tag.id}>
-                {tag.label}
-              </Badge>
-            ))}
+    <>
+      <Row className="align-items-center mb-4">
+        <Col>
+          <h1>{note.title}</h1>
+          {note.tags.length > 0 && (
+            <Stack gap={1} direction="horizontal" className="flex-wrap">
+              {note.tags.map((tag) => (
+                <Badge className="text-truncates" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <NavLink to={`/${note.id}/edit`}>
+              <Button variant="primary">Edit</Button>
+            </NavLink>
+            <Button variant="outline-danger">Delete</Button>
+            <NavLink to="/">
+              <Button variant="outline-dark">Back</Button>
+            </NavLink>
           </Stack>
-        )}
-      </Col>
-    </Row>
+        </Col>
+      </Row>
+      <ReactMarkdown>{note.markdown}</ReactMarkdown>
+    </>
   );
 };
 

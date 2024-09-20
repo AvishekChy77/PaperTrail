02b8721d9d1b864c876cc3dde7d2879a33b4901c
AvishekChy77 <avishekchowdhury71@gmail.com>
2024-09-20 23:59:41 +0600

    added navbar

--- src/Layout/Root.tsx
+++ src/Layout/Root.tsx
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import Navbar from "../Component/Navbar";
 
 const Root = () => {
   return (
     <Container className="my-4">
+      <Navbar />
       <Outlet />
     </Container>
   );
